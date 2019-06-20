642936afdf8fbd1f070ad0030028116126ae5b49
S Shahrukh <37923161+shahrukhsyed@users.noreply.github.com>
2019-06-20 17:40:45 +0530

    Update Validator.java

--- WalletProject_collection/src/com/cg/service/Validator.java
+++ WalletProject_collection/src/com/cg/service/Validator.java
@@ -3,8 +3,8 @@ package com.cg.service;
 public interface Validator {
 	String aidpattern="[1-9][0-9][0-9]";
 	String mobilepattern="[1-9]{1}[0-9]{9}";
-	String balancepattern="[1-9][0-9][0-9][0-9]*";
-	String namepattern="[A-Za-z\\s][A-Za-z\\s]*";
+	String balancepattern="[1-9][0-9][0-9][0-9]*+[.]*+[0-9]*";
+	String namepattern="[A-Z][a-z]*+[']*[]*+[a-z]*+[]*+[a-zA-Z]*";
 	
 	public static boolean validatedata(String data,String pattern) {
 		return data.matches(pattern);
